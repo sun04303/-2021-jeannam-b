fc5d4a81fea2a81d0125424ec4df426cf5bdb497
sun04303 <sin243996@gmail.com>
2021-09-24 20:52:38 +0900

    2021-09-24 19:52

--- resource/js/stamp.js
+++ resource/js/stamp.js
@@ -1,4 +1,12 @@
 let code = [], product = []
+let cardName
+
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+
+let cardImage = new Image()
+cardImage.src = './resource/img/stamp/stamp.png'
+cardImage.onload = () => {ctx.drawImage(cardImage, 0, 0)}
 
 fetch('./resource/js/code.json')
 .then(res => res.json())
@@ -12,4 +20,30 @@ fetch('./resource/js/product.json')
 .then(data => {
     product = data
     console.log(product)
+})
+
+$('.downOpen').on('click', e => {
+    $('#download').modal('show')
+})
+
+$('.card-d').on('click', e => {
+    cardName = $('#cardName').val()
+    if(cardName) {
+        ctx.fillText(cardName, 365, 20)
+        document.querySelector('.card-d').href = canvas.toDataURL()
+        $('#download').modal('hide')
+    } else {
+        e.preventDefault()
+        alert('이름을 입력해주세요')
+    }
+})
+
+$('.cardOpen').on('click', e => {
+    if(code.indexOf($('.codeInput').val()) == -1) {
+        alert('잘못된 코드입니다.')
+        $('.codeInput').val('')
+        return
+    }
+
+    $('#stampCard').modal('show')
 })
