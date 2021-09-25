c5e82e6c6de83ce6269b62e2da5b64b52e1520b0
sun04303 <sin243996@gmail.com>
2021-09-25 16:55:19 +0900

    2021-09-25 16:55

--- resource/js/stamp.js
+++ resource/js/stamp.js
@@ -4,9 +4,8 @@ let cardName
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
 
-let cardImage = new Image()
-cardImage.src = './resource/img/stamp/stamp.png'
-cardImage.onload = () => {ctx.drawImage(cardImage, 0, 0)}
+let mark = new Image()
+mark.src = './resource/img/stamp/mark.png'
 
 fetch('./resource/js/code.json')
 .then(res => res.json())
@@ -24,11 +23,15 @@ fetch('./resource/js/product.json')
 
 $('.downOpen').on('click', e => {
     $('#download').modal('show')
+    let cardImage = new Image()
+    cardImage.src = './resource/img/stamp/stamp.png'
+    cardImage.onload = () => {ctx.drawImage(cardImage, 0, 0)}
 })
 
 $('.card-d').on('click', e => {
     cardName = $('#cardName').val()
     if(cardName) {
+        ctx.fillStyle = '#fff'
         ctx.fillText(cardName, 365, 20)
         document.querySelector('.card-d').href = canvas.toDataURL()
         $('#download').modal('hide')
@@ -47,3 +50,40 @@ $('.cardOpen').on('click', e => {
 
     $('#stampCard').modal('show')
 })
+
+let view = document.querySelector('.stampview')
+let viewctx = view.getContext('2d')
+
+$('#cardSelect').on('change', e => {
+    let reader = new FileReader()
+    reader.onload = event => {
+        let cardImg = new Image()
+
+        cardImg.onload = () => {
+            viewctx.drawImage(cardImg, 0, 0)
+
+            console.log(viewctx.getImageData(10, 78, 1, 1))
+            console.log(viewctx.getImageData(70, 78, 1, 1))
+
+            viewctx.drawImage(mark, 20, 78) // x = 20 + 103, y = 78, 173
+            viewctx.drawImage(mark, 123, 78)
+            viewctx.drawImage(mark, 226, 78)
+            viewctx.drawImage(mark, 20, 173)
+
+            drawCircle(163, '#f00')
+            drawCircle(178, '#0f0')
+        }
+
+        cardImg.src = event.target.result
+    }
+
+    reader.readAsDataURL(e.target.files[0])
+})
+
+function drawCircle (x, color) {
+    viewctx.beginPath();
+    viewctx.arc(x, 271, 3, 0, Math.PI*2) // 첫 번째 163
+    viewctx.stroke();
+    viewctx.fillStyle = color
+    viewctx.fill()
+}
